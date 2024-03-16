40f4788e5d8e45901f47fb354fa1acc9fccaf93a
Andrii Dubovyk <and.dubovyk@gmail.com>
2024-03-16 13:43:38 +0200

    Add epmpty input/output fucntions

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def input_text_console():
+    pass
+
+
+def read_file_builtin(file_path):
+    pass
+
+
+def read_file_pandas(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,10 @@
+def output_text_console(text):
+    pass
+
+
+def write_file_builtin(file_path, content):
+    pass
+
+
+def write_file_pandas(file_path, content):
+    pass
